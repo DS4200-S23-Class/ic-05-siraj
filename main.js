657b088ce769f4c6ad75d8c7cbeb970bff1ce306
Marco6666662 <1419699960@qq.com>
2023-01-30 16:22:22 -0500

    updated

--- main.js
+++ main.js
@@ -49,11 +49,13 @@ function buttonClicked() {
 	console.log("button was clicked!");
 
 	count ++;
+	txt = "Number of times button clicked: "
+	let concat = txt.concat(count);
 
 	//let newText = "Button was clicked";
 
 	// select the element
 	let buttonDiv = document.getElementById("button-div");
 
-	buttonDiv.innerHTML = count;
+	buttonDiv.innerHTML = concat;
 }
